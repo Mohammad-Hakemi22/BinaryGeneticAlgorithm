847989e74e55686baee2021c3b66967eb327c022
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-03-06 09:10:09 +0330

    add mutation, fitnessfunc, binary2decimal, decimal2real

--- bga.py
+++ bga.py
@@ -1,4 +1,6 @@
 import numpy as np
+from operator import xor
+import math
 
 
 class BGA():
@@ -29,3 +31,35 @@ class BGA():
         assert(len(new_0) == len(new_1))
 
         return new_0, new_1
+
+    def mutation(self, pop):
+        num_mut = math.ceil(self.pm * pop.shape[0] * pop.shape[1])
+        for m in range(0, num_mut):
+            i = np.random.randint(0, pop.shape[0])
+            j = np.random.randint(0, pop.shape[1])
+            pop[i][j] = xor(pop[i][j], 1)
+        return pop
+
+    def fitnessFunc(self, real_val):
+        fitness_val = 21.5 + \
+            real_val[0]*np.sin(4*np.pi*real_val[0]) + \
+            real_val[1]*np.sin(20*np.pi*real_val[1])
+        return fitness_val
+
+    def b2d(self, list_b):
+        l = len(list_b)
+        sum = 0
+        for i in range(0, l):
+            p = ((l-1)-i)
+            sum += (pow(2, p) * list_b[i])
+        return sum
+
+    def d2r(self, b2d, lenght_b, m):
+        norm = b2d/(pow(2, lenght_b) - 1)
+        match m:
+            case 0:
+                real = self.low[0] + (norm * (self.high[0] - self.low[0]))
+                return real
+            case 1:
+                real = self.low[1] + (norm * (self.high[1] - self.low[1]))
+                return real
